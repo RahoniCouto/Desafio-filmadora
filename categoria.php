9de7fcaa0966c42a544cd326728103a67850272a
luanteosantos <luanteo.santos@gmail.com>
2020-01-18 11:27:26 -0300

    Inclusão da segunda pagina

---
+++ categoria.php
@@ -0,0 +1,73 @@
+<?php require_once('header.php'); ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Categorias</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="col-12">
+        <div class="row">
+            <h1 class="container" style="text-align: center">CATEGORIA</h1>
+        </div>
+    </div>
+
+
+
+    <div class="col-12">
+        <table class="table" style="width: 50%; margin: 0 auto">
+            <tbody style="text-align: center">
+                <tr>
+                    <td>FilmeX</td>
+                </tr>
+                <tr>
+                    <td>FilmeX</td>
+                </tr>
+                <tr>
+                    <td>FilmeX</td>
+                </tr>
+                <tr>
+                    <td>FilmeX</td>
+                </tr>
+                <tr>
+                    <td>FilmeX</td>
+                </tr>
+                <tr>
+                    <td>FilmeX</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="col-12">
+        <div class="row">
+            <button type="button" class="btn btn-primary" style="margin: 0 auto">VER TODOS OS FILMES</button>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
+
+
+<?php require_once('footer.php'); ?>
